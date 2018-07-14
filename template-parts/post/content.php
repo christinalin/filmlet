10e46010a281ae418d4fc0e2349079ce39d71600
Christina <christina.lin.yang@gmail.com>
2018-07-14 18:35:04 -0500

    refactor post cards on home page

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -16,72 +16,61 @@
 <!-- Homepage post -->
 <?php if ( is_home() ) : ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>
+		style="background:url('<?php the_post_thumbnail_url(); ?>') center / contain no-repeat">
 
-		<?php
+		<div class="post_banner">
+				<div class="post_banner_column_left">
+					<div class="post_thumb_container">
+					<a href="<?php the_permalink(); ?>">
+					<?php
+					  $mykey_values = get_post_custom_values( 'post_logo' );
+					  foreach ( $mykey_values as $key => $value ) {
+								echo "<img class='post_logo_img' src='/wp-content/themes/filmlet/assets/images/" . $value.  ".png' />";
+					  }
 
-		  $mykey_values = get_post_custom_values( 'post_logo' );
-		  foreach ( $mykey_values as $key => $value ) {
-		    // echo "$key  => $value ( 'post_logo' )<br />";
-				echo "<img class='post_logo' src='/wp-content/themes/filmlet/assets/images/" . $value.  ".png' />"; //. "$key  => $value ( 'post_logo' )<br />";
-		  }
+					?>
+					</a>
+				</div>
+				</div> <!-- post_banner_column_left -->
 
-		?>	<?php
-		if ( is_sticky() && is_home() ) :
-			echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
-		endif;
-		?>
-
-
-
-
-		<?php if ( '' !== get_the_post_thumbnail() && ! is_single() ) : ?>
-			<div class="post-thumbnail">
-				<a href="<?php the_permalink(); ?>">
-					<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
-				</a>
-			</div><!-- .post-thumbnail -->
-		<?php endif; ?>
-
-		<header class="entry-header">
-			<?php
-			if ( 'post' === get_post_type() ) {
-					if ( ! is_single() ) {
-						// twentyseventeen_edit_link();
+				<div class="post_banner_column_right">
+				<header class="entry-header">
+					<?php
+					if ( 'post' === get_post_type() ) {
+							if ( ! is_single() ) {
+								// twentyseventeen_edit_link();
+							};
 					};
-			};
 
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} elseif ( is_front_page() && is_home() ) {
-				the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-			?>
-		</header><!-- .entry-header -->
+					if ( is_single() ) {
+						the_title( '<h1 class="entry-title">', '</h1>' );
+					} elseif ( is_front_page() && is_home() ) {
+						the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+					} else {
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					}
+					?>
+				</header><!-- .entry-header -->
 
 
-		<div class="entry-content">
-			<?php
+				<div class="entry-content">
+					<?php
 
-				$mykey_values = get_post_custom_values( 'post_subtext' );
-				foreach ( $mykey_values as $key => $value ) {
-					// echo "$key  => $value ( 'post_logo' )<br />";
-					echo  $value;
-				}
+						$mykey_values = get_post_custom_values( 'post_subtext' );
+						foreach ( $mykey_values as $key => $value ) {
+							echo  $value;
+						}
 
-			?>
-		</div><!-- .entry-content -->
+					?>
+				</div><!-- .entry-content -->
+			</div> <!-- post_banner_column_right -->
 
-		<?php
-		if ( is_single() ) {
-			twentyseventeen_entry_footer();
-		}
-		?>
-
+		</div> <!-- post_banner -->
 	</article><!-- #post-## -->
 
+
+<!--  not the Home Page -->
 <?php else : ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
@@ -89,8 +78,7 @@
 
 		  $mykey_values = get_post_custom_values( 'post_logo' );
 		  foreach ( $mykey_values as $key => $value ) {
-		    // echo "$key  => $value ( 'post_logo' )<br />";
-				echo "<img class='post_logo' src='/wp-content/themes/filmlet/assets/images/" . $value.  ".png' />"; //. "$key  => $value ( 'post_logo' )<br />";
+				echo "<img class='post_logo' src='/wp-content/themes/filmlet/assets/images/" . $value.  ".png' />";
 		  }
 
 		?>	<?php
@@ -134,7 +122,6 @@
 
 				$mykey_values = get_post_custom_values( 'post_subtext' );
 				foreach ( $mykey_values as $key => $value ) {
-					// echo "$key  => $value ( 'post_logo' )<br />";
 					echo  $value;
 				}
 
