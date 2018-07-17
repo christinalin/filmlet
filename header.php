976a7863b7ebfc7a10d6fdd3bb4d1ddaa9a841bf
Christina <christina.lin.yang@gmail.com>
2018-07-16 19:00:14 -0500

    add css link and update icons

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 
